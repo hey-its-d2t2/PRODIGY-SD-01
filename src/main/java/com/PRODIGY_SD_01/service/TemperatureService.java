22b7f445456d5c43f2cfc89e9d33d56d0d746b96
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-12 13:20:14 +0530

    Temperature service interface methods decleared.

--- src/main/java/com/PRODIGY_SD_01/service/TemperatureService.java
+++ src/main/java/com/PRODIGY_SD_01/service/TemperatureService.java
@@ -1,4 +1,10 @@
 package com.PRODIGY_SD_01.service;
 
+import com.PRODIGY_SD_01.model.Temperature;
+
 public interface TemperatureService {
+    Temperature convertToFahrenheit(Temperature temp);
+    Temperature convertToCelsius(Temperature temp);
+    Temperature convertToKelvin(Temperature temp);
+    Temperature convertToReaumur(Temperature temp);
 }
